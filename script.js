463cbf937a011b4eed810c46a74c2da52427f75e
tiinker <tiffany.arellano@outlook.com>
2017-12-06 20:44:20 -0800

    add "before" option on image hover.

--- script.js
+++ script.js
@@ -11,7 +11,25 @@ this.addEventListener("change", function(){
     border-radius:${borderRad.value}%`; 
 });
 
+img.addEventListener("mouseenter", function(){
+    img.style = `filter: 
+    brightness(100%) 
+    blur(0px) 
+    grayscale(0%) 
+    contrast(100%) 
+    saturate(1);
+    border-radius:0%`;
+});
 
+img.addEventListener("mouseleave", function(){
+    img.style = `filter: 
+    brightness(${brightness.value}%) 
+    blur(${blur.value}px) 
+    grayscale(${grayscale.value}%) 
+    contrast(${contrast.value}%) 
+    saturate(${saturate.value});
+    border-radius:${borderRad.value}%`;
+});
 
 
 
